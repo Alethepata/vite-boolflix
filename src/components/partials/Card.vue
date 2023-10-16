5ff08536c347f2f329421c6d67c5cf59822675ca
Alethepata <alessia.patane@gmail.com>
2023-10-16 23:53:13 +0200

    [Add] Card

---
+++ src/components/partials/Card.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'Card',
+    props: {
+        original_title:String,
+        title:String,
+        original_language:String,
+        vote_average:String,
+    }
+
+}
+</script>
+
+<template>
+<div class="card w-25 text-center">
+    <h1>{{ title }}</h1>
+    <h3>{{ original_title }}</h3>
+    <p>{{ original_language }}</p>
+    <p>{{ vote_average }}</p>
+</div>
+</template>
+
+<style>
+
+</style>

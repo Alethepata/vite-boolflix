d6c2bfd8e791a5c4ed2780a08b756af305fc17fe
Alethepata <alessia.patane@gmail.com>
2023-10-16 23:52:41 +0200

    [Add] CardsContainer

---
+++ src/components/CardsContainer.vue
@@ -0,0 +1,34 @@
+<script>
+import { store } from '../data/store';
+import Card from './partials/Card.vue';
+
+export default {
+    name: 'CardsContainer',
+    components: {
+        Card
+    },
+    data() {
+        return {
+            store
+        }
+    }
+
+}
+</script>
+
+<template>
+    <div class="container d-flex flex-wrap">
+        <Card
+        v-for="movie in store.moviesList"
+        :key="movie.id"
+        :original_title="movie.original_title"
+        :title="movie.title"
+        :original_language="movie.original_language"
+        :vote_average="movie.vote_average"
+        />
+    </div>
+</template>
+
+<style>
+
+</style>

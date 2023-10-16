0702932ec2f6831ac532131f576b90b384658a36
Alethepata <alessia.patane@gmail.com>
2023-10-16 23:53:00 +0200

    [Add] SearchBar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<script>
+import { store } from '../data/store';
+export default {
+    name: 'SearchBar',
+    data() {
+        return {
+            store
+        }
+    }
+
+}
+</script>
+
+<template>
+    <div class="container text-end my-5">
+        <input v-model="store.movieToSearch" @keyup.enter="$emit('startSearch')" type="text" placeholder="Cerca">
+        <button @click="$emit('startSearch')" class="btn btn-info ms-3"> Search</button>
+    </div>
+</template>
+
+<style>
+
+</style>

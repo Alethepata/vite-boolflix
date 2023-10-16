a3c949cf24e6ce899c2e9ccfa074d6250cca5245
Alethepata <alessia.patane@gmail.com>
2023-10-16 23:51:24 +0200

    [Add] App

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,50 @@
 <script>
+
+import { store } from './data/store';
+import axios from 'axios';
+
+import SearchBar from './components/SearchBar.vue';
+import CardsContainer from './components/CardsContainer.vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+
+    SearchBar,
+    CardsContainer
+    
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    getApi() {
+      axios.get(store.apiUrl, {
+        params: {
+          query:store.movieToSearch
+        }
+      })
+        .then(res => {
+        store.moviesList = res.data.results
+      })
+    }
+  },
+  mounted() {
+    this.getApi()
+  }
 
 }
 </script>
 
 <template>
-  
+  <SearchBar @startSearch="getApi"/>
+  <CardsContainer/>
 </template>
 
-<style>
+<style lang="scss">
+
+@use './scss/main.scss'
 
 </style>

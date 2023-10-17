b5c57e5a07b4171bfd17edc2051b8a0df67cf9a6
Alethepata <alessia.patane@gmail.com>
2023-10-17 16:02:15 +0200

    [Edit] img

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -8,14 +8,14 @@ export default {
         name:String,
         original_language:String,
         vote_average: String,
-        backdrop_path:String
+        poster_path:String
     },
     methods: {
         getFlag(img) {
             return new URL (`../../assets/flag/${img}.png`, import.meta.url).href
         },
         getUrlImg(img) {
-             return `https://image.tmdb.org/t/p/w1280${img}`
+             return `https://image.tmdb.org/t/p/w342${img}`
   
         }       
 
@@ -26,21 +26,34 @@ export default {
 
 <template>
     <div class="col my-3">
-        <div class="card text-center h-100 ">
-            <h1>{{ title || name }}</h1>
-            <h3>{{ original_title || original_name }}</h3>
-            <img class="flag" :src="getFlag(original_language)" :alt="original_language">
-            <p v-if="vote_average.toFixed(0) < 5">{{ vote_average.toFixed(0) }}</p>
-            <p v-else>5</p>
-            <img :src="getUrlImg(backdrop_path)" :alt="title || name">
+
+        <div class="card text-center">        
+            <div class="card-img w-100 h-100">            
+                <img class="w-100 h-100 object-fit-cover" :src="getUrlImg(poster_path)" :alt="title || name">
+            </div>
+            <div class="card-text d-none">
+                <h1>{{ title || name }}</h1>
+                <h3>{{ original_title || original_name }}</h3>
+                <img class="flag" :src="getFlag(original_language)" :alt="original_language">
+                <p v-if="vote_average.toFixed(0) < 5">{{ vote_average.toFixed(0) }}</p>
+                <p v-else>5</p>
+            </div>
+
         </div>
     </div>
 </template>
 
 <style lang="scss">
 
-.flag{
-    width: 50px;
+.col{
+
+    .card{    
+        height: 500px;
+        overflow: auto;
+        .flag{
+        width: 50px;
+        }
+    }
 }
 
 

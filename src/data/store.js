e27837436a4f735439e096429a4c5c29fde47f25
Alethepata <alessia.patane@gmail.com>
2023-10-17 19:48:21 +0200

    [Add] popular

--- src/data/store.js
+++ src/data/store.js
@@ -2,7 +2,10 @@ import { reactive } from "vue";
 
 export const store = reactive({
     apiUrl: 'https://api.themoviedb.org/3/search/',
+    popularUrl: 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
     movie: [],
     tv: [],
-    movieToSearch:''
+    popular:[],
+    movieToSearch: '',
+    counter:0
 })

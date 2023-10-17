583e700f88aea370b22449509eb64f3bec3d8406
Alethepata <alessia.patane@gmail.com>
2023-10-17 13:23:40 +0200

    [Add] tv

--- src/data/store.js
+++ src/data/store.js
@@ -1,7 +1,8 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    apiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=b01a5b39baec6aab0a375381ad7bd179',
-    moviesList: [],
+    apiUrl: 'https://api.themoviedb.org/3/search/',
+    movie: [],
+    tv: [],
     movieToSearch:''
 })
